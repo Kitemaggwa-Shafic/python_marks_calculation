851915e68d4f468b98134985a9ae10a671ceb125
KitemaggwaShafic <kitemaggwashafic@gmail.com>
2023-04-12 19:07:38 +0300

    SHAFIC PYTHON ASSIGNMENTS My Assignements done in class students marks cal Assignment2_today.py This is a more update of first Assignment has while loop

---
+++ assignment2_today.py
@@ -0,0 +1,50 @@
+def testCompare(tests1,tests2):
+    tests1 = int(input('Please Enter Test One Marks Results : ' ))
+    tests2 = int(input('Please Enter Test Two Marks Results : '))
+    if tests1 >= tests2:
+        return tests1
+    else:
+        return tests2
+
+def courseWorkResult():
+    bestDone = testCompare(tests1, tests2)
+    avgTests = (bestDone + courseWorkMark )/2
+    avgTestFinal = avgTests * 0.4
+    courseWorkMark = int(input('Please Course Work Marks Results : ' ))
+    return avgTestFinal
+
+def examMarks():
+    avgexamMark = (courseWorkResult() + testCompare(tests1,tests2)) /2
+    examMark = avgexamMark * 0.6
+    return examMark
+
+#GEt
+while True:
+    try:
+        studentName = str(input('Please Student Name : ' ))
+        break
+    except ValueError:
+        print('Please characters : ' )
+'''
+studentNames = str(input('Please Enter Student Names : ' )) 
+studentAcademicYear = str(input('Please Enter Student Acadmic Year : ' ))
+courseUnitName = str(input('Please Course Unit Name : ' ))
+courseUnitCode = str(input('Please Course Unit Code : ' ))
+'''
+
+# Tests, course work, exam marks
+while True:
+    try:
+        # tests1, tests2 = testCompare(tests1, tests2) 
+        courseWorkMark = print(courseWorkResult())
+        if testCompare() < 20 and testCompare() >100:
+            raise ValueError
+        break
+    except ValueError:
+        print('Please test values are not below 20 and not above 100 : ' )
+        
+
+
+
+
+ 
